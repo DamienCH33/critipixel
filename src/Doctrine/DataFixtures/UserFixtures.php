4d0b5b6d7300f708b8bcadd83cff6d439ae57c73
DamienCH33 <damienchauveau@hotmail.fr>
2026-01-15 10:46:22 +0100

    maj user fixture

--- src/Doctrine/DataFixtures/UserFixtures.php
+++ src/Doctrine/DataFixtures/UserFixtures.php
@@ -10,10 +10,15 @@ final class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $users = \array_fill_callback(0, 10, fn (int $index): User => (new User())
+        $existing = (new User())
+            ->setEmail('existing@email.com')
+            ->setUsername('existingUser')
+            ->setPlainPassword('password');
+
+        $users = array_fill_callback(0, 10, fn (int $index): User => (new User())
             ->setEmail(sprintf('user+%d@email.com', $index))
-            ->setPlainPassword('password')
             ->setUsername(sprintf('user+%d', $index))
+            ->setPlainPassword('password')
         );
 
         array_walk($users, [$manager, 'persist']);
